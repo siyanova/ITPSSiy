552dc917c78c94157a5a7358d7e70e76b4afd8b2
Danya3564 <nw.finn@yandex.ru>
2023-12-19 12:05:18 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITPSSiy
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void PokupatButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new regist();
+            
+        }
+
+        private void MarktButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new redactormark();
+        }
+
+        private void SotrudnikButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new redactorSotrud();
+        }
+
+        private void AutoButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new redactorAuto();
+        }
+
+        private void SalesButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content=new redactorSales();
+        }
+    }
+}
